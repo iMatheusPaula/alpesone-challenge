8ee0c280ac524b3e800485b05e72aab9f19d743b
iMatheusPaula <imatheuspaula@gmail.com>
2025-08-27 18:01:40 -0300

    new example test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,18 @@
 <?php
 
-test('the application returns a successful response', function () {
-    $response = $this->get('/');
+use App\Models\User;
 
-    $response->assertStatus(200);
+test('can create a user in the database', function () {
+    $userData = [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => bcrypt('password'),
+    ];
+
+    $user = User::query()->create($userData);
+
+    expect($user)
+        ->toBeInstanceOf(User::class)
+        ->and($user->name)->toBe('Test User')
+        ->and($user->email)->toBe('test@example.com');
 });
