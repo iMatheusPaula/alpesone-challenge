58fd2bb499501f3e6c669214a3401df5b61d465f
iMatheusPaula <imatheuspaula@gmail.com>
2025-08-29 00:11:27 -0300

    fix cron on entrypoint

--- docker-entrypoint.prod.sh
+++ docker-entrypoint.prod.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 set -e
 
+# Espera o MySQL estar disponível para executar as migrations
 until php artisan migrate --force --no-interaction; do
   echo "🥱 mysql is not yet available..."
   sleep 2
 done
 
+# Configurações de cache para produção
 php artisan config:cache
-
 php artisan route:cache
 
+# Configura o cron job para rodar o scheduler do Laravel a cada minuto
 echo '* * * * * cd /var/www && php artisan schedule:run >> /var/log/cron.log 2>&1' > /etc/cron.d/laravel-schedule
 chmod 0644 /etc/cron.d/laravel-schedule
 crontab /etc/cron.d/laravel-schedule
 
-service cron start
+# Inicia o cron
+cron
 
 exec "$@"
