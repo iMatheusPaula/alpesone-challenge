c76a98fced57fba13deb0d93b6f96f72ad750b4a
iMatheusPaula <imatheuspaula@gmail.com>
2025-08-29 01:13:55 -0300

    refactor car routes to use route model binding for consistency

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,13 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/cars', [CarController::class, 'index']);
-Route::get('/cars/{id}', [CarController::class, 'show']);
+Route::get('/cars/{car}', [CarController::class, 'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::post('/cars', [CarController::class, 'store']);
-    Route::put('/cars/{id}', [CarController::class, 'update']);
-    Route::delete('/cars/{id}', [CarController::class, 'destroy']);
+    Route::put('/cars/{car}', [CarController::class, 'update']);
+    Route::delete('/cars/{car}', [CarController::class, 'destroy']);
 });
